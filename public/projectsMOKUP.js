78b1270f331d40038e04c1e351b4e67ac486572a
kani0dev <allanchagasilva@gmail>
2025-11-20 03:54:54 -0300

    modal refactor not good enogh yet

--- public/projectsMOKUP.js
+++ public/projectsMOKUP.js
@@ -2,42 +2,16 @@ const Projects = [
   {
     "name": "portifolio",
     "description" : "este é um site portifoli feito em react",
+    "repoLink": "https://github.com/kani0dev/Meu-Portifolio.git",
+    "url": null,
     "languagues" : [
-      "javascript"
+      "javascript",
+        "c",
+        "v",
     ],
     "framework" : [
       "react", "ant-design"
     ]
-  },
-  {
-    "name": "sistema-gestao",
-    "description": "Sistema interno de gestão empresarial com relatórios integrados",
-    "languagues": ["java", "sql"],
-    "framework": ["Spring Boot", "Hibernate", "Thymeleaf"]
-  },
-  {
-    "name": "app-clima",
-    "description": "Aplicativo mobile de previsão do tempo com geolocalização",
-    "languagues": ["dart"],
-    "framework": ["flutter"]
-  },
-  {
-    "name": "analise-dados",
-    "description": "Ferramenta de análise e visualização de dados para negócios",
-    "languagues": ["python", "javascript"],
-    "framework": ["django", "pandas", "d3.js"]
-  },
-  {
-    "name": "jogo-educativo",
-    "description": "Jogo educativo para crianças com foco em matemática básica",
-    "languagues": ["c#"],
-    "framework": ["unity"]
-  },
-  {
-    "name": "api-segura",
-    "description": "API robusta com autenticação e autorização avançadas",
-    "languagues": ["go"],
-    "framework": ["gin", "jwt", "redis"]
   }
 ]
 
--- src/assets/componets/LanguagesSection/modal/LanguagesModal.jsx
+++ src/assets/componets/LanguagesSection/modal/LanguagesModal.jsx
@@ -1,31 +1,72 @@
-import {getProjectByLanguage} from "/public/projectsMOKUP.js";
+import { getProjectByLanguage } from "/public/projectsMOKUP.js";
 import "./Style.css";
 
-import { Modal, List, Tag, Typography, Space, Divider } from "antd";
+import {Modal, List, Tag, Typography, Space, Card, Flex, Divider} from "antd";
+import { GithubOutlined  } from "@ant-design/icons";
+import Link from "antd/es/typography/Link.js";
 
-const { Title, Text } = Typography;
+const { Text, Title } = Typography;
 
 export default function CriarLanguagesModal({ lang, onClose }) {
-    const projetos = getProjectByLanguage(lang)
+    const projetos = getProjectByLanguage(lang);
+
+    const ProjectTags = ({  items}) =>
+        items?.length > 0 && (
+            <Space  wrap >
+                {items.map((i) => (
+                    <Tag key={i} >
+                        {i}
+                    </Tag>
+                ))}
+            </Space>
+        );
+
     return (
         <Modal
-            open={true}
+            open
             onCancel={onClose}
             footer={null}
-            title={`Projetos em ${lang}`}
+            centered
+            width={650}
+            title={<Title style={{fontSize:"4"}}>Projetos que utilizam {lang}</Title>}
         >
             <List
                 dataSource={projetos}
+                locale={{ emptyText: <Text>Nenhum projeto encontrado utilizando {lang}.</Text> }}
                 renderItem={(proj) => (
-                    <>
-                        <List.Item>
-                            <List.Item.Meta
-                                title={<Title level={5}>{proj.name}</Title>}
+                    <List.Item style={{ padding: 0, borderBottom: "none" }}>
+                        <Card style={{ width: "100%"}}>
+                            <Flex vertical >
+                                <Flex justify={"space-between"} horizontal align={"center"}>
+                                    <Divider strong orientation={"start"} style={{ fontSize: 18 }}>
+                                        {proj.name}
+                                    </Divider>
+                                        <Link
+                                            href={proj.repoLink}
+                                            target="_blank"
+                                            onClick={(e) => e.stopPropagation()}
+                                        >
+                                            <GithubOutlined />
+                                        </Link>
 
-                                description={<Text type="secondary">{proj.description}</Text>}
-                            />
-                        </List.Item>
-                    </>
+                                </Flex>
+                                <Text type="secondary">{proj.description}</Text>
+
+                                <Text strong >Tecnologias utilizadas:</Text>
+                                <Flex horrizontal style={{ marginTop: 10 }}>
+                                    <ProjectTags
+                                        items={proj.languagues}
+                                        color="blue"
+                                    />
+                                    <ProjectTags
+                                        items={proj.framework}
+                                        color="green"
+                                    />
+                                </Flex>
+                            </Flex>
+                        </Card>
+
+                    </List.Item>
                 )}
             />
         </Modal>
