dcc89c8e5d8dfc5c62cb319a5488e911a7399645
kani0dev <allanchagasilva@gmail.com>
2026-09-21 11:27:22 -0300

    courses

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import LanguagesBTNS from "./assets/componets/LanguagesSection/btn/LinguagemsBTN.jsx";
 import CriarLanguagesModal from './assets/componets/LanguagesSection/modal/LanguagesModal.jsx';
-import { Typography, Divider, Space, Card, Flex, Row, Col } from 'antd';
+import { Typography, Divider, Space, Card, Flex, Row, Col, List } from 'antd';
 import Aboutme from './assets/componets/Aboutme/Aboutme.jsx';
 import ProjectLangContainer from './assets/componets/LanguagesSection/index.jsx';
 import ProjectCard from './assets/componets/ProjectsSection/ProjectCard.jsx';
@@ -10,28 +10,45 @@ import ProjectCard from './assets/componets/ProjectsSection/ProjectCard.jsx';
 const { Title, Paragraph, Text } = Typography;
 
 function App() {
-
+const courses = [ 
+  'Foundadations of Cybersecurity - google - coursera',
+  'Google Cloud Computing Foundations - google - coursera'
+]
  return (
-  <Row
-  align="middle"
-  >
-    <Col
-    span={6}
-    />
-    <Col 
-      span={12}
-    >
-      <Flex
-      vertical
-      >
-        <Aboutme/>
-        
-        <ProjectLangContainer/>
-      </Flex>
-    </Col>
-    <Col
-    span={6}
-    />
+  <Row align="middle">
+    <Col span={6}/>
+      <Col span={12}>
+        <Flex vertical>
+    
+          <Aboutme/>
+    
+          <ProjectLangContainer/>
+
+            <Card
+              title="Experiencias"
+            >          
+            </Card>
+
+            <Card
+              title="Cursos"
+            >
+              <List
+                bordered
+                dataSource={courses}
+                renderItem={(item)=>(
+                <List.Item>
+                  <Typography.Text>
+                    {item}
+                  </Typography.Text>
+                </List.Item>
+                )}
+              />
+            </Card>
+
+
+        </Flex>
+      </Col>
+    <Col span={6}/>
   </Row>
   );
 }; 
